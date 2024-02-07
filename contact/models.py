1d008196a0c65d012d46afccbf77b70216da435d
DiegoSousaOliveira <diegosousa0056@gmail.com>
2024-02-07 12:57:44 -0300

    criando um usuário

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Category(models.Model):
+  class Meta:
+    verbose_name = 'Category'
+    verbose_name_plural = 'Categories'
   name = models.CharField(max_length=50)
 
   def __str__(self) -> str:
@@ -20,7 +24,12 @@ class Contact(models.Model):
     Category,
     on_delete=models.SET_NULL,
     blank=True, null=True
-  )
+    )
+  owner = models.ForeignKey(
+    User,
+    on_delete=models.SET_NULL,
+    blank=True, null=True
+    )
 
   def __str__(self) -> str:
     return f'{self.first_name} {self.last_name}'
