857ba68c778d9c962d29bf8e26815ea35bc0fc50
DiegoSousaOliveira <diegosousa0056@gmail.com>
2024-02-09 12:47:45 -0300

    barra de pesquisa

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 
 from contact.models import Contact
 
@@ -16,6 +16,27 @@ def index(request):
     'contact/index.html',
     context
   )
+def search(request):
+  search_value = request.GET.get('q', '').strip()
+
+  if search_value == '':
+    return redirect('contact:index')
+
+  contacts = Contact.objects \
+    .filter(show=True) \
+    .order_by('-id')[10:20]
+
+  context = {
+    'contacts': contacts,
+    'site_title': 'Contatos - '
+  }
+  return render(
+    request, 
+    'contact/index.html',
+    context
+  )
+
+
 
 def contact(request, contact_id):
   #single_contact = Contact.objects.filter(pk=contact_id).first()
