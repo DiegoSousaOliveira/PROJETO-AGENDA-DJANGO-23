c54980f4d73fbc80b3de860282eee0add87210b4
DiegoSousaOliveira <diegosousa0056@gmail.com>
2024-02-10 07:26:58 -0300

    Create contact_forms.py

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+from contact.forms import ContactForm
+
+
+def create(request):
+  if request.method == 'POST':
+    context = {
+      'form': ContactForm(request.POST)
+    }
+
+    return render(
+      request, 
+      'contact/create.html',
+      context
+  )
+
+  context = {
+      'form': ContactForm()
+    }
+
+  return render(
+    request, 
+    'contact/create.html',
+    context
+  )
