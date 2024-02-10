2b8601424be9ce9c6213a7457eba129c90ce4f1b
DiegoSousaOliveira <diegosousa0056@gmail.com>
2024-02-10 07:27:01 -0300

    Update contact_views.py

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.db.models import Q
+from django.core.paginator import Paginator
+
 from contact.models import Contact
 
 def index(request):
   contacts = Contact.objects \
     .filter(show=True) \
-    .order_by('-id')[10:20]
+    .order_by('-id')
+  
+  paginator = Paginator(contacts, 10)
+  page_number = request.GET.get('page')
+  page_obj = paginator.get_page(page_number)
 
   context = {
-    'contacts': contacts,
+    'page_obj': page_obj,
     'site_title': 'Contatos - '
   }
   return render(
@@ -16,6 +22,8 @@ def index(request):
     'contact/index.html',
     context
   )
+
+
 def search(request):
   search_value = request.GET.get('q', '').strip()
 
@@ -31,17 +39,20 @@ def search(request):
       Q(email__icontains=search_value)
             ) \
     .order_by('-id')
+  
+  paginator = Paginator(contacts, 10)
+  page_number = request.GET.get('page')
+  page_obj = paginator.get_page(page_number)
 
   context = {
-    'contacts': contacts,
-    'site_title': 'Contatos - '
+    'page_obj': page_obj,
+    'site_title': 'Search - ',
   }
   return render(
     request, 
     'contact/index.html',
     context
   )
-
 
 
 def contact(request, contact_id):
