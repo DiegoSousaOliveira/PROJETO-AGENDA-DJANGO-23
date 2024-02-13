936ffffb0f59b062c4b0bf5e3806f3c6c9141688
DiegoSousaOliveira <diegosousa0056@gmail.com>
2024-02-13 13:56:02 -0300

    Create user_forms.py

---
+++ contact/views/user_forms.py
@@ -0,0 +1,51 @@
+from django.contrib import auth, messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+
+from contact.forms import ResgisterForm
+
+
+def register(request):
+  form = ResgisterForm()
+
+  if request.method == 'POST':
+    form = ResgisterForm(request.POST)
+
+    if form.is_valid():
+      form.save()
+      messages.success(request, 'Usuário registrado')
+      return redirect('contact:login')
+      
+  return render(
+    request,
+    'contact/register.html',
+    {
+      'form': form
+    }
+  )
+
+def login_view(request):
+  form = AuthenticationForm(request)
+
+  if request.method == "POST":
+    form = AuthenticationForm(request, data=request.POST)
+
+    if form.is_valid():
+      user = form.get_user()
+      auth.login(request, user)
+      messages.success(request, 'Logado com sucesso!')
+      return redirect('contact:index')
+    
+    messages.error(request, 'Login inválido!')
+
+  return render(
+    request,
+    'contact/login.html',
+    {
+      'form': form
+    }
+  )
+
+def logout_view(request):
+  auth.logout(request)
+  return redirect('contact:login')
